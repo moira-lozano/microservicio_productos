3576d5f2c91a3511aba9facc085f6f00b61dc3bb
Moira Genesis Lozano Arauz <moira_la@outlook.es>
2024-05-21 15:30:39 -0400

    Instalacion de la libreria Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
