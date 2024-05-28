5ac9a11d69ce84da5c4f6a344044bdd0c10ad474
Moira Genesis Lozano Arauz <moira_la@outlook.es>
2024-05-28 12:47:16 -0400

    CRUD de compra-detalle completo y funciona bien. Instalacion de graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,6 +11,9 @@ import { CompraController } from './compra/compra.controller';
 import { CompraService } from './compra/compra.service';
 import { Compra } from "./compra/compra.model";
 import { CompraDetalleModule } from './compra_detalle/compra_detalle.module';
+import { CompraDetalle } from "./compra_detalle/compra_detalle.model";
+import { CompraDetalleService } from "./compra_detalle/compra_detalle.service";
+import { CompraDetalleController } from "./compra_detalle/compra_detalle.controller";
 
 @Module({
   imports: [
@@ -21,16 +24,17 @@ import { CompraDetalleModule } from './compra_detalle/compra_detalle.module';
       username: 'postgres', 
       password: 'moira123', 
       database: 'db_microservicio', 
-      entities: [Producto, Compra], // Importa tu modelo Producto aquí
+      entities: [Producto, Compra, CompraDetalle], // Importa tu modelo Producto aquí
       synchronize: true,
     }),
-    TypeOrmModule.forFeature([Producto, Compra]), // Asegúrate de importar tu modelo Producto aquí también
+    TypeOrmModule.forFeature([Producto, Compra, CompraDetalle]), // Asegúrate de importar tu modelo Producto aquí también
     ProductoModule, CompraModule, CompraDetalleModule,
   ],
-  controllers: [ProductoController, CompraController],
+  controllers: [ProductoController, CompraController, CompraDetalleController],
   providers: [
     ProductoService,
     CompraService,
+    CompraDetalleService,
     {
       provide: APP_FILTER,
       useClass: NotFoundExceptionFilter, //para manejar las excepciones
--- src/compra_detalle/compra_detalle.controller.ts
+++ src/compra_detalle/compra_detalle.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Put, Delete, NotFoundException } from '@nestjs/common';
+import { CompraDetalleService} from "./compra_detalle.service";
+import { CompraDetalle } from "./compra_detalle.model";
 
 @Controller('compra-detalle')
-export class CompraDetalleController {}
+export class CompraDetalleController {
+    constructor (private compraDetalleService: CompraDetalleService) {}
+
+    @Get()
+    async findAll(): Promise<CompraDetalle[]> {
+      return this.compraDetalleService.findAll();
+    }    
+  
+    @Get(':id')
+    async findOne(@Param('id') id: number): Promise<CompraDetalle> {
+      const compras_por_id = await this.compraDetalleService.findOne(id);
+      if (!compras_por_id) {
+        throw new NotFoundException("Purchase details does not exist");
+      }
+      return compras_por_id;
+    }
+  
+    @Post()
+    async create(@Body() purchase: CompraDetalle): Promise<CompraDetalle> {
+      return this.compraDetalleService.create(purchase);
+    }
+  
+    @Put(':id')
+    async update(@Param('id') id: number, @Body() purchase: CompraDetalle): Promise<CompraDetalle> {
+      return this.compraDetalleService.update(id, purchase);
+    }
+  
+    @Delete(':id')
+    async remove(@Param('id') id: number): Promise<void> {
+      try {
+        return await this.compraDetalleService.remove(id);
+      } catch (error) {
+        throw new NotFoundException("Purchase details does not exist");
+        
+      }
+    }
+}
